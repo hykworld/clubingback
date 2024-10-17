a37543c7f70e01109b5080e024dffd6d733896f2
DESKTOP-BL58UVV\H <hyk910@gmail.com>
2024-10-17 12:44:35 +0900

    ㅁ

--- backend/index.js
+++ backend/index.js
@@ -114,7 +114,6 @@ const kakao = require("./src/routes/kakao");
 app.use("/kakao", kakao);
 const startServer = async () => {
   try {
-    console.log("MongoDB 연결 시도 중...");  // 연결 시도 로그 추가
     await mongoose.connect(process.env.MONGO_URI);
     //await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log("몽고디비 연결 완료");
